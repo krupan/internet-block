263b98ce78ac81a33cc6cd618525a546056bb8af
Bryan Murdock <bmurdock@gmail.com>
2020-10-16 22:24:20 -0600

    moved everything that I could to ansible

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-install_dir=/home/pi/internet-block
-etterfilter $install_dir/drop-all.ecf -o $install_dir/drop-all.ef
-sudo cp $install_dir/*.timer /etc/systemd/system/
-sudo cp $install_dir/*.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl restart internet-start.timer
-sudo systemctl restart internet-stop.timer
--- package-and-copy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-git archive --format=tar.gz --prefix=internet-block/ HEAD > internet-block.tar.gz
-rsync -a internet-block.tar.gz pi@10.20.30.5:
-ssh pi@10.20.30.5 mv -i internet-block internet-block.$(date --iso-8601=seconds)
-ssh pi@10.20.30.5 tar xvf internet-block.tar.gz
